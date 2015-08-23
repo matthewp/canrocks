82bf1d06b4f4f6010d1b6678a44e88e7939935d6
Matthew Phillips <matthew@matthewphillips.info>
2015-08-23 09:10:02 -0400

    home and search routing works

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 var route = require("can/route/");
 require("can/route/pushstate/");
 
-route(":page", { page: "home" });
+route(":page", { page: "home", query: null });
+route(":page?q=:query");
 
 var env = require("@loader").env;
 if(env === "development" && location.port != 8080) {
