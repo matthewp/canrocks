00cde9578d8a2bd2fc7bb010585996f040c0c2c1
Matthew Phillips <matthew@matthewphillips.info>
2015-09-09 21:02:58 -0400

    Update tests

--- src/models/component_test.js
+++ src/models/component_test.js
@@ -6,7 +6,7 @@ QUnit.module('models/component');
 QUnit.test('getList', function(){
   stop();
   Component.getList().then(function(items) {
-    QUnit.equal(items.length, 3);
+    QUnit.equal(items.length, 4);
     QUnit.equal(items.attr('0.description'), 'A basic tabs component');
     start();
   });
