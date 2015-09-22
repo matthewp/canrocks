2c90f5eec96854b01e135edcde9c4180944855fa
Matthew Phillips <matthew@matthewphillips.info>
2015-09-21 22:04:10 -0400

    Another crack at the deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 COMMIT=`git rev-list -n1 master`
-ssh -i key.priv $SERVER -oStrictHostKeyChecking=no bin/update_canrocks $COMMIT
+chmod 0600 key.priv
+ssh -i key.priv matthew@$SERVER -oStrictHostKeyChecking=no bin/update_canrocks $COMMIT
