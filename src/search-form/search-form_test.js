601523e7b1c0dfe00c062f6ce6b50c2333ccac9d
Matthew Phillips <matthew@matthewphillips.info>
2015-10-05 22:15:54 -0400

    Adds a search-form test
    This adds a search-form test. Tests that when submitting a search the
    @root viewModel's `query` property is set to the search term typed in.

--- src/search-form/search-form_test.js
+++ src/search-form/search-form_test.js
@@ -1,10 +1,28 @@
-import QUnit from 'steal-qunit';
-import { ViewModel } from './search-form';
+var QUnit = require("steal-qunit");
+var ViewModel = require("./search-form.component!").ViewModel;
+var Map = require("can/map/");
+var stache = require("can/view/stache/");
+var $ = require("jquery");
+var F = require("funcunit");
+
+F.attach(QUnit);
 
 // ViewModel unit tests
-QUnit.module('search-form');
+QUnit.module("<search-form>", {
+  setup: function(){
+    this.root = new Map();
+    var tmpl = stache("<search-form></search-form>");
+    $("#qunit-test-area").html(tmpl(this.root))
+  }
+});
 
-QUnit.test('Has message', function(){
-  var vm = new ViewModel();
-  QUnit.equal(vm.attr('message'), 'This is the search-form component');
+QUnit.test("Updates the searchTerm property on the root viewModel", function(){
+  F(".search-term").exists().type("canjs").blur();
+  F(".submit-button").click();
+
+  var root = this.root;
+  F(function(){
+    var term = root.attr("query");
+    QUnit.equal(term, "canjs", "The query was set on the root viewModel");
+  });
 });
