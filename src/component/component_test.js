afd5febb07a44c7e4590824e08b3eef206df11ef
Matthew Phillips <matthew@matthewphillips.info>
2016-02-09 21:19:06 -0500

    Revert "all tests are passing"
    This reverts commit 3b77f0eecbd34ee45ed398310251a3078e27307b.

--- src/component/component_test.js
+++ src/component/component_test.js
@@ -14,7 +14,7 @@ QUnit.module('<component-page> email/gravatar', {
     this.vm = new ViewModel({
       packageName: "testPackage",
       component: {
-        maintainers: {username: "matthewp", email: "matthew@matthewphillips.info" }
+        maintainers: "matthewp <matthew@matthewphillips.info>"
       }
     });
   }
